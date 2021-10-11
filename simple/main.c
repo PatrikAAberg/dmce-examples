19bb8ad5f11ede69aeacc9692603493be351ffbf
Patrik Aberg <patrik.aberg@ericsson.com>
2021-10-11 16:46:47 +0200

    simple: first version

---
+++ simple/main.c
@@ -0,0 +1,15 @@
+#include "simple.h"
+
+int main() {
+
+    int a = 0;
+    int b = 0;
+    int c;
+
+    c = simple(a, &b);
+    c += simple(a, &b);
+    c += simple(a, &b);
+
+    return c;
+}
+
---
+++ simple/simple.c
@@ -0,0 +1,5 @@
+int simple(int a, int* b) {
+
+    return a + *b;
+}
+
